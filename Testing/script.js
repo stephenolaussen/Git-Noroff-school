d4af5854682459a0bf29edfdafca6e926aa2a224
Stephenolaussen <Stephenolaussen@gmail.com>
2025-09-24 12:42:13 +0200

    Update Testing project files
    - Modified HTML structure
    
    - Updated JavaScript functionality
    
    - Improved CSS styling

--- Testing/script.js
+++ Testing/script.js
@@ -1,2 +1,2 @@
-console.log("Hello, World!!"); // This is a simple JavaScript file for testing purposes
+console.log("Hello, Stephen"); // This is a simple JavaScript file for testing purposes
 alert("This is a test alert!");
